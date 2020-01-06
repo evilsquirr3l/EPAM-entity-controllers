62d0e6dbdd44dd9e30bb6693c8b6e349e57398cb
evilsquirrel <x.brothers.sp@gmail.com>
2020-01-07 01:27:20 +0200

    Minor update

--- DAL/Entities/Product.cs
+++ DAL/Entities/Product.cs
@@ -5,11 +5,9 @@ namespace EPAM_entity.Entities
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public double Price { get; set; }
-
-        public int CategoryId { get; set; }
         public Category Category { get; set; }
-
-        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
         public Supplier Supplier { get; set; }
+        public int SupplierId { get; set; }
     }
 }
